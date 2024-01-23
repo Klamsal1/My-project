4447500020fad50913537e8772a2bb414b7ddd77
Kedar, Lamsal <kedarl@gmail.com>
2024-01-23 14:07:21 -0600

    The second add

---
+++ School.java
@@ -0,0 +1,23 @@
+public class School {
+
+     public void add ( int x , int y){
+
+         int z = x + y;
+
+         System.out.println( z);
+     }
+
+     public double add ( double x, double y){
+
+         System.out.println( " The sum is  " + x+y);
+         return x + y;
+     }
+
+    public static void main(String[] args) {
+        School s = new School();
+        s.add(5,7);
+        s.add(5.4,6.7);
+
+    }
+
+}
